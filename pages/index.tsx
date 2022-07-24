103c30478a6c5ac8051a3a1817fc11f14aba996d
anx234 <navcrk5@gmail.com>
2022-07-24 22:36:52 +0900

    layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,39 @@ import type { NextPage } from 'next'
 import { Layout } from './../layouts/Layout'
 const Home: NextPage = () => {
   return (
-    <Layout>
     <section className="mt-16">
-      <h1 className="text-7xl font-bold">Hi I'm <span className="dark:text-purple-600">Cand</span></h1>
-      <h3 className="text-4xl my-3">I am Web Designer</h3>
-      <p className="text-gray-700 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti quibusdam autem doloremque beatae iure, nihil fugit doloribus cum soluta modi!</p>
+          <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="p-8 rounded shadow-xl sm:p-16">
+        <div className="flex flex-col lg:flex-row">
+          <div className="mb-6 lg:mb-0 lg:w-1/2 lg:pr-5">
+            <h2 className="font-sans text-3xl font-bold tracking-tight sm:text-4xl sm:leading-none">
+              The quick, brown fox
+              <br className="hidden md:block" />
+              jumps over{' '}
+              <span className="inline-block text-deep-purple-accent-400">
+                a lazy dog
+              </span>
+            </h2>
+          </div>
+          <div className="lg:w-1/2">
+            <p className="mb-4 text-baseßßß">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque rem aperiam, eaque ipsa quae. Sed ut
+              perspiciatis unde omnis iste. Sed ut perspiciatis unde omnis iste
+              error sit voluptatem accusantium doloremque rem aperiam.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
     </section>
-    </Layout>
   )
 }
 
